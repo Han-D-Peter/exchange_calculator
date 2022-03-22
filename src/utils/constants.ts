a8105094842b5d22b7c16f076901b099bd6824a1
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-03-22 23:08:11 +0900

    Feat: 매직스트링 변수 정리

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,11 @@
-export {}
+export const USD = 'USD';
+export const AUD = 'AUD';
+export const KRW = 'KRW';
+export const JPY = 'JPY';
+export const PHP = 'PHP';
+
+export const sendingCountries = [USD, AUD];
+
+export const receivingCountries = [KRW, JPY, PHP];
+
+export const countriesForGetRate = [USD, AUD, KRW, JPY, PHP];
