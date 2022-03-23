577ed83685b087b1924afbc3abed36665f003b23
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-03-23 18:18:49 +0900

    Fix: api env 배포 단계 테스트

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const getExchangeRate = (countries: Array<string>) => {
   const params = countries.length ? `&currencies=${countries.join()}` : '';
+  console.log(process.env.REACT_APP_API_KEY);
   return axios.get(
     `http://api.currencylayer.com/live?access_key=${process.env.REACT_APP_API_KEY}${params}`,
   );
