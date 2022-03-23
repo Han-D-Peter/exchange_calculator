3a1801b12810e27552cbe921e3cd9b9df095cb98
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2022-03-23 18:43:18 +0900

    Fix: http api block 문제 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const getExchangeRate = (countries: Array<string>) => {
   const params = countries.length ? `&currencies=${countries.join()}` : '';
   return axios.get(
-    `http://api.currencylayer.com/live?access_key=${process.env.REACT_APP_API_KEY}${params}`,
+    `https://api.currencylayer.com/live?access_key=${process.env.REACT_APP_API_KEY}${params}`,
   );
 };
 
